68b1d90c77b0d42e9cd1435e97b84f6a5ae50e95
Mateus Sarmento <omateusmento@gmail.com>
2022-12-06 11:33:26 -0300

    Fix import of Sprint Repository

--- src/sprints/sprints.module.ts
+++ src/sprints/sprints.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { SprintsService } from './sprints.service';
 import { SprintsController } from './sprints.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Entity } from 'typeorm';
+import { Sprint } from './entities/sprint.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Entity])],
+  imports: [TypeOrmModule.forFeature([Sprint])],
   controllers: [SprintsController],
   providers: [SprintsService],
 })
