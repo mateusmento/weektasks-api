a67f5bf6bf8ddcde269850915af275bacee1ac82
Mateus Sarmento <omateusmento@gmail.com>
2022-12-10 08:54:53 -0300

    Create BacklogItem entity

--- src/backlog-items/entities/backlog-item.entity.ts
+++ src/backlog-items/entities/backlog-item.entity.ts
@@ -1,1 +1,21 @@
-export class BacklogItem {}
+import { Epic } from 'src/epics/entities/epic.entity';
+import { Issue } from 'src/issues/entities/issue.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class BacklogItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'double' })
+  order: number;
+
+  @Column()
+  type: 'issue' | 'epic';
+
+  @ManyToOne(() => Issue)
+  issue: Issue;
+
+  @ManyToOne(() => Epic)
+  epic: Epic;
+}
