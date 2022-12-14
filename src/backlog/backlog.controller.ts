0f612e345eb3962e579e99b76698d85ce3e5dbeb
Mateus Sarmento <omateusmento@gmail.com>
2022-12-13 22:39:40 -0300

    Add GET /backlog/issues-end-epics endpoint

--- src/backlog/backlog.controller.ts
+++ src/backlog/backlog.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('backlog')
-export class BacklogController {}
+export class BacklogController {
+  @Get('issues-and-epics')
+  findIssuesAndEpics() {
+    return [];
+  }
+}
