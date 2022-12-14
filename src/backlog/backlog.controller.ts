97dd69943098abe1571967010d033dc91ccdd8b4
Mateus Sarmento <omateusmento@gmail.com>
2022-12-13 22:41:11 -0300

    Inject BacklogService in BacklogController

--- src/backlog/backlog.controller.ts
+++ src/backlog/backlog.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { BacklogService } from './backlog.service';
 
 @Controller('backlog')
 export class BacklogController {
+  constructor(private backlogService: BacklogService) {}
+
   @Get('issues-and-epics')
   findIssuesAndEpics() {
     return [];
