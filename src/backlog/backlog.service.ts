681b3bcf3c72b75bee45e9d16e61475291d5e36d
Mateus Sarmento <omateusmento@gmail.com>
2022-12-13 22:40:07 -0300

    Inject issue and epic repositories

--- src/backlog/backlog.service.ts
+++ src/backlog/backlog.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Epic } from 'src/epics/entities/epic.entity';
+import { Issue } from 'src/issues/entities/issue.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class BacklogService {}
+export class BacklogService {
+  constructor(
+    @InjectRepository(Issue)
+    private issueRepo: Repository<Issue>,
+    @InjectRepository(Epic)
+    private epicRepo: Repository<Epic>,
+  ) {}
+}
