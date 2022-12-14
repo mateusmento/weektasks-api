f2d77322d6e8eb96ab19173121efeccd1828e254
Mateus Sarmento <omateusmento@gmail.com>
2022-12-13 22:37:45 -0300

    Add BacklogController

--- src/backlog/backlog.module.ts
+++ src/backlog/backlog.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EpicsModule } from 'src/epics/epics.module';
 import { IssuesModule } from 'src/issues/issues.module';
+import { BacklogController } from './backlog.controller';
 
 @Module({
   imports: [IssuesModule, EpicsModule],
+  controllers: [BacklogController],
 })
 export class BacklogModule {}
